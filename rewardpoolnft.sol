0835b6cf6b7674c1e8085891feea1c0e474119c2
Daniel <contact@danielgoldman.us>
2024-10-02 07:54:31 -0400

    Fixed error in mintTo

--- rewardpoolnft.sol
+++ rewardpoolnft.sol
@@ -455,7 +455,7 @@ contract RewardPoolNFT is ERC721Enumerable, Ownable {
 
     function mintTo(address _to, uint256 _count) public onlyOwner nonReentrant {
         for (uint256 i = 0; i < _count; i++) {
-            _safeMint(msg.sender, nextTokenId);
+            _safeMint(_to, nextTokenId);
             ClaimManager(payable(claimManager)).initializeNFT(nextTokenId);
             nextTokenId += 1;
         }
