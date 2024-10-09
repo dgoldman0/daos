1ca94b8957c5dd7591be88b5fc4540526d26d743
Daniel <contact@danielgoldman.us>
2024-10-09 18:45:29 -0400

    Compiles

--- rewardnft/games/mancala/mancala.sol
+++ rewardnft/games/mancala/mancala.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "../../utils/ownable.sol";
 
 // Will create a game NFT as well and a token representing ownership of that game. It'll include a copy of the final game state, the players, as well as each turn.abi
