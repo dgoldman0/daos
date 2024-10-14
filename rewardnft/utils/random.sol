e53a004a5925a4ffc95c75d36c6168c30860bbf0
Daniel <contact@danielgoldman.us>
2024-10-14 06:30:30 -0400

    Still doesn't update fast enough

--- rewardnft/utils/random.sol
+++ rewardnft/utils/random.sol
@@ -24,16 +24,25 @@ contract RandomSeed is Ownable {
     constructor() {
         // Valid for Arbitrum
         factory = IUniswapV3Factory(0x1F98431c8aD98523631AE4a59f267346ea31F984);
-        // Add the UNI-LINK pool with 3000 fee tier (Arbitrum)
-        pools.push(Pool(0xf97f4df75117a78c1A5a0DBb814Af92458539FB4, 0xFa7F8980b0f1E64A2062791cc3b0871572f1F7f0, 3000));
-
+        // Add the LINK-UNI pool with 3000 fee tier (Arbitrum)
+        pools.push(Pool(0xf97f4df75117a78c1A5a0DBb814Af92458539FB4, 0xFa7F8980b0f1E64A2062791cc3b0871572f1F7f0, 3000)); // Works
+        // Add ARB-UNI pool with 3000 fee tier (Arbitrum)
+        pools.push(Pool(0x912CE59144191C1204E64559FE8253a0e49E6548, 0xFa7F8980b0f1E64A2062791cc3b0871572f1F7f0, 3000)); // Works
+        // Add ARB-LINK pool with 3000 fee tier (Arbitrum)
+        pools.push(Pool(0x912CE59144191C1204E64559FE8253a0e49E6548, 0xf97f4df75117a78c1A5a0DBb814Af92458539FB4, 3000));
+        // Add USDC-USDT pool with 3000 fee tier (Arbitrum)
+        pools.push(Pool(0xaf88d065e77c8cC2239327C5EDb3A432268e5831, 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9, 3000));
+        // Might need to add in lower fee ones. 0.01% fee tier
+        pools.push(Pool(0xf97f4df75117a78c1A5a0DBb814Af92458539FB4, 0xFa7F8980b0f1E64A2062791cc3b0871572f1F7f0, 100));
+        pools.push(Pool(0x912CE59144191C1204E64559FE8253a0e49E6548, 0xFa7F8980b0f1E64A2062791cc3b0871572f1F7f0, 100));
+        pools.push(Pool(0xaf88d065e77c8cC2239327C5EDb3A432268e5831, 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9, 100));
     }
 
     // Get the sum of tickCumulatives for the most recent price
     function getSeed() public view returns (int256 tickSum) {
         int256 sum = 0;
         for (uint256 i = 0; i < pools.length; i++) {
-            sum += UINT256(getMostRecentTick(i));
+            sum += int256(getMostRecentTick(i));
         }
         return sum;
     }
