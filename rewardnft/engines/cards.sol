3ce2cc7bfb7436f614258c98b90eb7df1e8b6757
Daniel <contact@danielgoldman.us>
2024-10-14 16:05:51 -0400

    Added more functions

--- rewardnft/engines/cards.sol
+++ rewardnft/engines/cards.sol
@@ -9,6 +9,7 @@ interface IRandomSeedGenerator {
 
 contract DeckManager is Ownable {
     IRandomSeedGenerator public randomSeed;
+    address public deckAuthorizer;
 
     // Generic deck which can be linked to any tokens 1:1 through cardIds
     struct Deck {
@@ -23,8 +24,10 @@ contract DeckManager is Ownable {
     mapping(uint256 => Deck) public decks;
     uint256 nextDeckId = 1;
 
-    constructor(address _randomSeed) {
+    constructor(address _randomSeed) Ownable() {
         randomSeed = IRandomSeedGenerator(_randomSeed);
+        deckAuthorizer = msg.sender;
+
     }
 
     function addDeck(string memory name, uint256[] memory cardIds) external onlyOwner {
@@ -41,7 +44,47 @@ contract DeckManager is Ownable {
         nextDeckId++;
     }
 
-    function setRandomSeed(address _randomSeed) external onlyOwner {
+    // Get the cards in the deck
+    function getDeck(uint256 deckId) external view returns (uint256[] memory) {
+        return decks[deckId].cardIds;
+    }
+    // Get remaining cards in the deck
+    function getRemainingCards(uint256 deckId) external view returns (uint256[] memory) {
+        return decks[deckId].remainingCards;
+    }
+    // Get already chosen cards
+    function getChosenCards(uint256 deckId) external view returns (uint256[] memory) {
+        return decks[deckId].chosenCards;
+    }
+
+    // Choose a card at random. Consider that the random number may be anywhere between 0 and 2^128-1
+    function chooseCard(uint256 deckId) external returns (uint256) {
+        Deck storage deck = decks[deckId];
+        require(deck.remainingCards.length > 0, "No cards remaining");
+        uint256 random = randomSeed.getSeed();
+        uint256 index = random % deck.remainingCards.length;
+        uint256 chosenCard = deck.remainingCards[index];
+        deck.chosenCards.push(chosenCard);
+        deck.remainingCards[index] = deck.remainingCards[deck.remainingCards.length - 1];
+        deck.remainingCards.pop();
+        return chosenCard;
+    }
+
+    // Reset the deck to its original state (copy cardIds to remainingCards and clear chosenCards)
+    function resetDeck(uint256 deckId) external onlyOwner {
+        Deck storage deck = decks[deckId];
+        for (uint256 i = 0; i < deck.cardIds.length; i++) {
+            deck.remainingCards[i] = deck.cardIds[i];
+        }
+        deck.chosenCards = new uint256[](0);
+    }
+
+    // Set the random seed generator
+    function setRandomSeedGenerator(address _randomSeedGenerator) external onlyOwner {
         randomSeed = IRandomSeedGenerator(_randomSeed);
+    }
+
+    function setDeckAuthorizer(address _deckAuthorizer) external onlyOwner {
+        deckAuthorizer = _deckAuthorizer;
     }
 }
